cfc46f2d8854b21e657592535ea51eb7e6ed2be1
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2020-09-01 00:00:34 -0600

    added build folder

---
+++ build/precache-manifest.abdf1dac84388937503eb53526421cde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d586eecdedf1e92c342dd90ac717638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0b5b00ba716decd2faf",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "953041980c965b04de26",
+    "url": "/static/css/main.f084d89a.chunk.css"
+  },
+  {
+    "revision": "c0b5b00ba716decd2faf",
+    "url": "/static/js/2.d58b918a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.d58b918a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "953041980c965b04de26",
+    "url": "/static/js/main.d41d0cbb.chunk.js"
+  },
+  {
+    "revision": "9614e760d85e397489ec",
+    "url": "/static/js/runtime-main.88a90fbd.js"
+  }
+]);
