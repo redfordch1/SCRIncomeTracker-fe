07b8c3c94a267413385ec166178d326bd7a93da0
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2020-09-09 23:03:46 -0600

    ad

--- src/components/TotalCard.js
+++ src/components/TotalCard.js
@@ -2,9 +2,11 @@ import React from "react";
 import "../styles/totalCard.css";
 
 function TotalCard(props) {
+  const dt = props.date;
+  const newDT = dt.toLocaleString("en-US", { timeZone: "UTC" });
   return (
     <div className="main_card">
-      <p>{props.date}</p>
+      <p>{newDT}</p>
       <p>${props.totals}</p>
     </div>
   );
